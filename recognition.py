9093bfba1a4702346e4191d3cb853d93e3a36c99
Aris Spyrou <arisspyrou@hotmail.com>
2019-06-14 20:02:05 +0300

    improvements

--- recognition.py
+++ recognition.py
@@ -1,20 +1,20 @@
 import librosa
 import numpy as np
-from scipy import signal
 from scipy.io import wavfile
 from segmentation import preprocess
+import glob
 from dtw import dtw, accelerated_dtw
 
-prefix = "voice//"
-filename = "1234_filtered_seg_4"
+prefix = "voice//segmented//"
+filename = "F1-9"
 suffix = ".wav"
 SAVE = 0
 
 if __name__ == "__main__":
     sr, x = wavfile.read(prefix + filename + suffix)  # Load the audio clip with native sampling rate
     sr, x = preprocess(sr, x)
-    #b = signal.firwin(101, [500, 3500], fs=sr, pass_zero=False)  # Create a FIR band pass filter
-    #x = signal.lfilter(b, [1], x, axis=0)  # Apply the filter
+    # b = signal.firwin(101, [500, 3500], fs=sr, pass_zero=False)  # Create a FIR band pass filter
+    # x = signal.lfilter(b, [1], x, axis=0)  # Apply the filter
     NS = 10  # Window size in ms
     MS = 10  # Window offset in ms
     L = int(NS * (sr / 1000))  # Window size in samples
@@ -22,21 +22,26 @@ if __name__ == "__main__":
     feat = []
     i = 0
     while True:
-        feat.append(librosa.feature.mfcc(x[i*R:i*R+R].astype(float))[1:])
+        frame = np.multiply(x[i*R:i*R+L], np.hamming(L))
+        feat.append(librosa.feature.mfcc(frame.astype(float))[1:])
         i += 1
-        if len(x) - (i*R+R) == 0:
+        if len(x) - (i*R+L) == 0:
             break
-        elif len(x) - (i*R+R) < 0:
+        elif len(x) - (i*R+L) < 0:
+            frame = np.multiply(x[i*R:], np.hamming(len(x)-i*R))
             feat.append(librosa.feature.mfcc(x[i*R:].astype(float))[1:])
             break
     feat = np.array(feat)
+
     feat = np.reshape(feat, (feat.shape[0], feat.shape[1]))
     if SAVE:
         np.save(prefix+"mfcc//"+filename, feat)
     else:
         mat = []
-        for i in range(1, 8):
-            y = np.load("voice//mfcc//"+"raw1234567_seg_{}".format(i)+".npy")
+        for it in glob.glob(prefix + 'mfcc//*'):
+            if filename[:-1] in it:
+                continue
+            y = np.load(it)
             mat.append(accelerated_dtw(feat, y, dist='euclidean')[0])
-        print(mat.index(min(mat))+1)
+        print(mat.index(min(mat)))
 
--- segmentation.py
+++ segmentation.py
@@ -2,51 +2,42 @@ import librosa
 import numpy as np
 from scipy import signal
 from scipy.io import wavfile
-import os
 
-
-file = "voice//A09"
+file = "voice//M1"
 suffix = ".wav"
 
 
 def preprocess(sr, x):
-    x = x.T
     if x.shape[0] == 2:
         x = librosa.to_mono(x.astype(float))
     if sr != 8000:
         x = librosa.resample(x.astype(float), sr, 8000)
-    return 8000, x
+        sr = 8000
+    return sr, x
 
 
 def filter_audio(filename):
     sr, x = wavfile.read(filename + suffix)  # Load the audio clip with native sampling rate
     sr, x = preprocess(sr, x)
-    b = signal.firwin(101, [500, 3500], fs=sr, pass_zero=False)  # Create a FIR band pass filter
+    b = signal.firwin(101, [200, 3800], fs=sr, pass_zero=False)  # Create a FIR band pass filter
     x = signal.lfilter(b, [1], x, axis=0)  # Apply the filter
-    wavfile.write(filename + "_filtered" + suffix, sr, x.astype(np.int16))  # Save filtered as wav
+    wavfile.write(filename + "-f" + suffix, sr, x.astype(np.int16))  # Save filtered as wav
+    return x
 
 
 def segment(filename):
     suffix = ".wav"
-    if ".wav" in filename:
+    if suffix in filename:
         filename, suffix = filename.split('.')
         suffix = '.' + suffix
-    if "filtered" not in filename:
-        if os.path.exists(filename+"_filtered"+suffix):
-            filename = filename+"_filtered"
-        else:
-            filter_audio(filename)
-            filename = filename + "_filtered"
-    sr, x = wavfile.read(filename + suffix)  # Load the audio clip with native sampling rate
-    sr, x = preprocess(sr, x)
+    filter_audio(filename)
+    sr, x = wavfile.read(filename + "-f" + suffix)  # Load the audio clip
 
     NS = 10  # Window size in ms
     MS = 10  # Window offset in ms
     L = int(NS * (sr/1000))  # Window size in samples
     R = int(MS * (sr/1000))  # Window offset in samples
 
-    Zc = librosa.feature.zero_crossing_rate(x.astype(float), R, R)  # Calculate zero-crossing rate
-    Zc = np.reshape(np.multiply(Zc, R), (Zc.shape[1],))
     E = []
     i = 0
     while True:
@@ -60,13 +51,9 @@ def segment(filename):
             break
     eavg = np.mean(E[:10])
     esig = np.std(E[:10])
-    zcavg = np.mean(Zc[:10])
-    zcsig = np.std(Zc[:10])
 
-    IF = 35  # Fixed threshold for Zc
-    IZCT = max(IF, zcavg + 2*zcsig)  # Variable threshold for Zc
     IMX = -30
-    ITU = IMX - 20  # High threshold for E
+    ITU = IMX - 50  # High threshold for E
     ITL = max(eavg+3*esig, ITU-10)  # Low threshold for E
 
     end_idx = 0
@@ -103,15 +90,19 @@ def segment(filename):
             except IndexError:
                 end_idx = voice_pos[i][1]
                 ref_voice_pos.append([start_idx, end_idx])
-                print(ref_voice_pos)
+                # print(ref_voice_pos)
                 break
 
+    sr, x = wavfile.read(filename + suffix)
+    prefix, filename = filename.split('//')
+    prefix += '//segmented//'
     # Save the segmented voice clips using the unfiltered file as a base
     for i, pos in enumerate(ref_voice_pos):
-        sr, x = wavfile.read(file + suffix)
-        start_idx = pos[0]*R
-        end_idx = pos[1]*R
-        wavfile.write(file + "_seg_{}".format(i+1) + suffix, sr, x[start_idx:end_idx].astype(np.int16))
+        start_idx = pos[0] * R
+        end_idx = pos[1] * R
+        wavfile.write(prefix + filename + "-{}".format(i) + suffix, sr, x[start_idx:end_idx].astype(np.int16))
+
+    return ref_voice_pos
 
 
 if __name__ == "__main__":
