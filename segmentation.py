36cde1c7d409d3ba451f0995d7028a88ecdf06ea
Aris Spyrou <arisspyrou@hotmail.com>
2019-06-15 18:28:03 +0300

    final

--- segmentation.py
+++ segmentation.py
@@ -2,27 +2,36 @@ import librosa
 import numpy as np
 from scipy import signal
 from scipy.io import wavfile
+from matplotlib import pyplot as plt
 
-file = "voice//M1"
+file = "voice//M2"
 suffix = ".wav"
+
+SAVE_FIL = 0  # Save filtered audio to file
+SAVE_SEGS = 1  # Save segmented audio clips for debugging and evaluation
 
 
 def preprocess(sr, x):
-    if x.shape[0] == 2:
-        x = librosa.to_mono(x.astype(float))
+    try:
+        if x.shape[1] == 2:
+            x = x.T
+            x = librosa.to_mono(x.astype(float))
+    except IndexError:
+        pass
     if sr != 8000:
         x = librosa.resample(x.astype(float), sr, 8000)
         sr = 8000
     return sr, x
 
 
-def filter_audio(filename):
+def filter_audio(filename, band_pass):
     sr, x = wavfile.read(filename + suffix)  # Load the audio clip with native sampling rate
     sr, x = preprocess(sr, x)
-    b = signal.firwin(101, [200, 3800], fs=sr, pass_zero=False)  # Create a FIR band pass filter
+    b = signal.firwin(101, band_pass, fs=sr, pass_zero=False)  # Create a FIR band pass filter
     x = signal.lfilter(b, [1], x, axis=0)  # Apply the filter
-    wavfile.write(filename + "-f" + suffix, sr, x.astype(np.int16))  # Save filtered as wav
-    return x
+    if SAVE_FIL:
+        wavfile.write(filename + "-f" + suffix, sr, x.astype(np.int16))  # Save filtered as wav
+    return sr, x
 
 
 def segment(filename):
@@ -30,8 +39,7 @@ def segment(filename):
     if suffix in filename:
         filename, suffix = filename.split('.')
         suffix = '.' + suffix
-    filter_audio(filename)
-    sr, x = wavfile.read(filename + "-f" + suffix)  # Load the audio clip
+    sr, x = filter_audio(filename, [200, 3800])
 
     NS = 10  # Window size in ms
     MS = 10  # Window offset in ms
@@ -48,13 +56,19 @@ def segment(filename):
             E = np.array(E)
             E = 10*np.log(E)
             E = np.subtract(E, E.max())
+            #plt.plot(E)
+            #plt.show()
             break
-    eavg = np.mean(E[:10])
-    esig = np.std(E[:10])
+    os = 0
+    for val in E:
+        if val == -np.inf:
+            os += 1
+        else:
+            break
+    eavg = np.mean(E[os:10+os])
+    esig = np.std(E[os:10+os])
 
-    IMX = -30
-    ITU = IMX - 50  # High threshold for E
-    ITL = max(eavg+3*esig, ITU-10)  # Low threshold for E
+    ITL = max(eavg+3*esig, -60)  # Threshold for E
 
     end_idx = 0
     voice_pos = []
@@ -90,18 +104,16 @@ def segment(filename):
             except IndexError:
                 end_idx = voice_pos[i][1]
                 ref_voice_pos.append([start_idx, end_idx])
-                # print(ref_voice_pos)
                 break
 
     sr, x = wavfile.read(filename + suffix)
     prefix, filename = filename.split('//')
     prefix += '//segmented//'
+    ref_voice_pos = np.multiply(ref_voice_pos, R)
     # Save the segmented voice clips using the unfiltered file as a base
-    for i, pos in enumerate(ref_voice_pos):
-        start_idx = pos[0] * R
-        end_idx = pos[1] * R
-        wavfile.write(prefix + filename + "-{}".format(i) + suffix, sr, x[start_idx:end_idx].astype(np.int16))
-
+    if SAVE_SEGS:
+        for i, pos in enumerate(ref_voice_pos):
+            wavfile.write(prefix + filename + "-{}".format(i) + suffix, sr, x[pos[0]:pos[1]].astype(np.int16))
     return ref_voice_pos
 
 
